1f679057c48c433030810f6a3039be91cda3556a
Adrianne M <affirmwealth@gmail.com>
2014-07-16 02:30:36 -0500

    added additional pages About and Contact, added additional text, made changes to the navigation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/contact'
+
   resources :blogs
+
+  root :to => redirect('/blogs')
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
